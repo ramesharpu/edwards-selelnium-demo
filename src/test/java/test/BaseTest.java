6d6a21672c360fff466815604f6007599777dd71
Ramesh A <ramesharpu@gmail.com>
2024-09-12 00:45:45 +0530

    modified the browser configuration

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -4,18 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 	
 	//instance variable
-	public String browser = "chrome";
 	public String url = "https://www.facebook.com";
 	
 	//class variable
 	static WebDriver driver;
 	
+	@Parameters({"browser"})
 	@BeforeTest
-	public void setup() {
+	public void setup(String browser) {
 		//open the browser and url
 		driver = new ChromeDriver();
 		driver.get(url);
